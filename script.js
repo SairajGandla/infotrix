52dc88b8f9ee1d1448fbd9a5b2f9d83da34e775b
SairajGandla <91734931+SairajGandla@users.noreply.github.com>
2024-01-08 10:35:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const weather = document.getElementById("weather");
+const dropdown = document.getElementById("dropdown");
+
+weather.addEventListener("click", () => {
+  dropdown.classList.toggle("collapse");
+});
+const timer = setInterval(() => {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  document.getElementById("time").innerHTML = `${hours} : ${minutes} ${
+    hours >= 12 ? "PM" : "AM"
+  }`;
+}, 1000);
